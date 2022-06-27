1f06deaf4290b8b8d1e0d586d0fb566c8442f577
Kavyachittineni <107785319+Kavyachittineni@users.noreply.github.com>
2022-06-27 17:47:13 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+// import the scanner class
+class User{
+public static void main(String[] args){
+Scanner Scn = new Scanner(System.in);
+  //create a scanner object
+System.out.println("Enter username");
+String userName = Scn.nextLine();
+  //read user input
+System.out.println("User name:" +userName);
+  //output user input
+}
+}
