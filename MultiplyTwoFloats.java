254515a37292b3a797f5294380466af4728b5bf4
Kavyachittineni <107785319+Kavyachittineni@users.noreply.github.com>
2022-06-27 17:53:53 +0530

    MultiplyTwoFloats

---
+++ MultiplyTwoFloats.java
@@ -0,0 +1,7 @@
+class MultiplyTwoFloats{
+         public static void main(String args[]){
+	float num1 = 67.99009f;						
+	float num2 = 2.22f;						
+	System.out.println("Multiply two floats : "+ num1 * num2);
+	}
+}
